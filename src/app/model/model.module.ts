724da3fe0020a1392032df59248dc6adc4b13082
masterh4cker <tabathafoes@gmail.com>
2020-02-27 13:29:45 -0700

    1 file modified

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,13 +4,8 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
-import { RestDataSource } from "./rest.datasource";
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [HttpClientModule],
-  providers: [ProductRepository, Cart, Order, OrderRepository,
-    { provide: StaticDataSource, useClass: RestDataSource }]
+    providers: [ProductRepository, StaticDataSource, Cart]
 })
 export class ModelModule { }
-
